25662fd6fd01db094f5247fc0166ed4cd4480c2d
uriel <uriel@LAPTOP-S7K83OQV>
2024-01-04 18:05:28 +0100

    Aggiungere i file di progetto.

---
+++ vettori24.9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vettori24._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vettore v1 = new Vettore();
+            v1.X = 2;
+            v1.Y = 2;
+            v1.Calcolalunghezza();
+            Vettore v2 = new Vettore();
+            v2.X = 3;
+            v2.Y = 3;
+            v2.Calcolalunghezza();
+            Vettore v3 = new Vettore();
+            Console.WriteLine("ascisse vettore?");
+            v3.X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ordinate vettore?");
+            v3.Y = int.Parse(Console.ReadLine()); 
+            v3.Calcolalunghezza();
+            Console.WriteLine("primo vettore: ") ;
+            Console.WriteLine( (v1.X).ToString());
+            Console.WriteLine( (v1.Y).ToString());
+            Console.WriteLine("secondo vettore: ");
+            Console.WriteLine((v2.X).ToString());
+            Console.WriteLine((v2.Y).ToString());
+            Console.WriteLine("somma vettori: ");
+            Console.WriteLine((v1.Lunghezza + v2.Lunghezza).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ vettori24.9/Vettore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vettori24._9
+{
+    internal class Vettore
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public double Lunghezza { get; set; }
+        public double Calcolalunghezza() 
+        {
+            Lunghezza= Math.Sqrt(X*X/2+Y*Y/2);
+            return Lunghezza;
+        }
+        
+    }
+}
